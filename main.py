c55beb229e07c87833fe5593d2361019874ba8fe
Tal Berenstein <tal.berenstein@pm.me>
2022-05-22 23:28:29 +0200

    convert image 1.0 + cleaning

--- main.py
+++ main.py
@@ -1,23 +1,18 @@
-import logging
 
-# from warnings import filters
-from telegram import ext
-from telegram import ForceReply
 from telegram.ext import Updater
-from telegram.update import Update
 from telegram.ext.callbackcontext import CallbackContext
 from telegram.ext.commandhandler import CommandHandler
 from telegram.ext.messagehandler import MessageHandler
 from telegram.ext.conversationhandler import ConversationHandler
 from telegram.ext.filters import Filters
 import requests
-from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove, Update
+from telegram import ReplyKeyboardRemove, Update
 
-# updater = Updater(
-#     "5199045475:AAHBcN0_bQKNqswNAFPN2U5CgTfhUDYViiM", use_context=True)
+import json
+
 import Constants
 
-CONVERT, PHOTO, FORMAT_TO, BIO = range(4)
+CONVERT, PHOTO, FORMAT_TO, HANDLE_CONVERT = range(4)
 
 
 def start(update: Update, context: CallbackContext):
@@ -28,57 +23,51 @@ def start(update: Update, context: CallbackContext):
     return CONVERT
 
 
-
-
-def youtube_url(update: Update, context: CallbackContext):
-    update.message.reply_text("Youtube Link =>\
-	https://www.youtube.com/")
-
-
 def handleSendRequest(urlToConver):
     pass
 
 
 def photo(update: Update, context: CallbackContext) -> int:
     print("insert photo")
+    photo_file = update.message.photo[-1].get_file()
+    context.user_data["picture"] = photo_file.file_path
+    #print(photo_file.file_path)
+    # urlToConver = photo_file.file_path
+    # handleSendRequest(urlToConver)
+    update.message.reply_text(text="Please provide a type")
+    return FORMAT_TO
+    #return formatTo(photo_file.file_path ,update, context)
+    #update.message.reply_text(response.text)
+
+
+def formatTo(update: Update, context: CallbackContext) -> int:
+    #print("formatTo: ", photoUrl)
+    formatToConvert = update.message.text
+    picture = context.user_data["picture"]
+    print(formatToConvert)
+    print(picture)
     url = "https://image-processing4.p.rapidapi.com/imageconvert"
-    querystring = {
-        "imageUrl": "https://codingforum.site/img/logo.png", "outputType": "gif"}
 
     headers = {
         "X-RapidAPI-Host": "image-processing4.p.rapidapi.com",
         "X-RapidAPI-Key": Constants.API_KEY_SERVICE
     }
-    photo_file = update.message.photo[-1].get_file()
-    print(photo_file.file_path)
-    # urlToConver = photo_file.file_path
-    # handleSendRequest(urlToConver)
-    update.message.reply_text(text="Please provide a type")
-    return formatTo(update, context)
-    querystring = {"imageUrl": photo_file.file_path, "outputType": "gif"}
+    querystring = {"imageUrl": picture, "outputType": formatToConvert}
     response = requests.request(
         "GET", url, headers=headers, params=querystring)
 
-    print(response)
+    print(response.text)
+    result = json.loads(response.text)
+    update.message.reply_text(result["outPutUrl"])
+    return ConversationHandler.END
 
-    update.message.reply_text(response.text)
-
-
-def formatTo(update: Update, context: CallbackContext) -> int:
-    print("FORMAT_TO -> formatTo")
-    formatToConvert = update.message
-    print(formatToConvert)
-
+def handleConvert(queryString, update: Update, context: CallbackContext) -> int:
+    print("handleConvert")
 
 def convert(update: Update, context: CallbackContext):
     update.message.reply_text("""You selected /convert""")
     update.message.reply_text(text="Please provide a picture")
     return PHOTO
-
-
-def geeks_url(update: Update, context: CallbackContext):
-    update.message.reply_text(
-        "GeeksforGeeks URL => https://www.geeksforgeeks.org/")
 
 
 def unknown(update: Update, context: CallbackContext):
@@ -102,30 +91,23 @@ async def cancel(update: Update, context: CallbackContext) -> int:
     return ConversationHandler.END
 
 
-# updater.dispatcher.conv_handler(ConversationHandler(
-# 	entry_points=[CommandHandler("start", start)],
-# 	states={
-# 		PHOTO: [MessageHandler(filters.PHOTO, photo)]
-# 	}
-# ))
-
 def main() -> None:
     """Run the bot"""
     updater = Updater(
-        "5199045475:AAHBcN0_bQKNqswNAFPN2U5CgTfhUDYViiM", use_context=True)
+        Constants.TELEGRAM_KEY_SERVICE, use_context=True)
     conv_handler = ConversationHandler(
         entry_points=[CommandHandler("start", convert)],
         states={
             # GENDER: [MessageHandler(filters.Regex("^(Boy|Girl|Other)$"), gender)],
             CONVERT: [],
             PHOTO: [MessageHandler(Filters.photo, photo),
-                    formatTo],
-            # FORMAT_TO: [MessageHandler(Filters.text, formatTo), formatTo]
+                    MessageHandler(Filters.regex("^(svg|jpg|png)$"), formatTo)],
+            FORMAT_TO: [MessageHandler(Filters.text, formatTo), handleConvert],
+            #HANDLE_CONVERT: [handleConvert]
             # LOCATION: [
             #     MessageHandler(filters.LOCATION, location),
             #     CommandHandler("skip", skip_location),
             # ],
-            # BIO: [MessageHandler(filters.TEXT & ~filters.COMMAND, bio)],
         },
         fallbacks=[CommandHandler("cancel", cancel)],
     )
@@ -143,14 +125,6 @@ if __name__ == "__main__":
 #     }
 # ))
 
-# updater.dispatcher.add_handler(CommandHandler('start', start))
-# updater.dispatcher.add_handler(CommandHandler('youtube', youtube_url))
-# updater.dispatcher.add_handler(CommandHandler('help', help))
-# updater.dispatcher.add_handler(CommandHandler('convert', convert))
-# updater.dispatcher.add_handler(CommandHandler('geeks', geeks_url))
-# updater.dispatcher.add_handler(
-#     MessageHandler(Filters.photo, photo))
-# updater.dispatcher.add_handler(MessageHandler(
 #     Filters.command, unknown))  # Filters out unknown commands
 
 # Filters out unknown messages.
